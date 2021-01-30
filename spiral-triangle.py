d8dd1e9277584bd4fbba6064e7a355652fede027
DrKrause <61912389+DrKrause@users.noreply.github.com>
2021-01-31 03:11:32 +0600

    added spiral-rectangle

---
+++ spiral-triangle.py
@@ -0,0 +1,18 @@
+import turtle 
+
+def triangle(length):
+    turtle.hideturtle()
+    turtle.color('green')
+    turtle.pensize(5)
+    turtle.Screen().bgcolor("black")
+    for i in range(3):
+        for j in range(10):
+            turtle.forward(length)
+            length += 13
+            turtle.left(120)
+
+length = 32
+gens = 10
+
+triangle(length)
+turtle.Screen().exitonclick()
